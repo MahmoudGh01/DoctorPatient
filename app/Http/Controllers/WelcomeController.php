981b86c261e0fc99e4fdb7872f4edc82e9520b94
MahmoudGh01 <80069718+MahmoudGh01@users.noreply.github.com>
2025-11-27 20:33:30 +0100

    Refactor welcome page layout and controller
    Changed WelcomeController to use the __invoke method instead of index. Simplified the welcome.blade.php view by replacing the <x-app-layout> with <x-site-layout> and removing the extensive inline HTML, CSS, and content, likely delegating layout and content responsibilities to the new layout component.

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -6,12 +6,11 @@ use Illuminate\Http\Request;
 
 class WelcomeController extends Controller
 {
-    function index()
+    function __invoke()
     {
-        //dd('Message from WelcomeController@index');
-        //dump('Message from WelcomeController@index');
 
         // Business logic can be added here if needed
+
 
         return view('welcome');
     }
